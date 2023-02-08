58aec1ec813437afe3a1d4a6759f1daca9cdad71
kalabamare88 <kalab_amare@yahoo.com>
2023-02-08 17:52:22 +0300

    login and register storage update

---
+++ pages/api/auth/[...roqAuth].js
@@ -0,0 +1,20 @@
+import { RoqAuth } from "@roq/nextjs";
+
+/*
+    You can export RoqAuth without passing any options if you don't need to customize the behaviour
+
+    export default RoqAuth; //handles all the authentication routes automatically
+*/
+
+export default RoqAuth({
+  hooks: {
+    // This hook is optional - and can be used to persist user information,
+    // or as in the case below, send them a welcome notification
+    onLoginSuccess({ session, user }) {
+      //   If the user was just created, welcome them
+      if (Date.now() - new Date(user.createdAt).getTime() < 60000) {
+        "wellcome"
+      }
+    },
+  },
+});
